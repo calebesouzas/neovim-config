b6448d20d03a6dd13e9a5f38e91e6f79aae420f4
calebesouzas <cacalebehs@gmail.com>
2025-12-21 11:49:19 -0300

    Basic mappings and indenting options

---
+++ init.lua
@@ -0,0 +1,19 @@
+-- // Leader Mappings // --
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- // Options // --
+-- Line numbers
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.cursorline = true
+
+-- Scrolling
+vim.o.scrolloff = 16
+
+-- Indentation
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.softtabstop = 2
+vim.o.expandtab = true
+vim.o.smartindent = true
