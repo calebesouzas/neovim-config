53e93c7a60a99b77776b42af8c2dd45d7e92b3ef
Calebe <swplayermc30@gmail.com>
2025-12-22 14:55:15 -0300

    Setup Lazy plugin manager

--- init.lua
+++ init.lua
@@ -17,3 +17,32 @@ vim.o.shiftwidth = 2
 vim.o.softtabstop = 2
 vim.o.expandtab = true
 vim.o.smartindent = true
+
+
+
+
+-- // Lazy Plugin Manager Config // --
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({
+    "git", "clone", "--filter=blob:none", "--branch=stable",
+    lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  spec = {
+  },
+  -- install = { colorscheme = { "habamax" } },
+  checker = { enabled = true },
+})
