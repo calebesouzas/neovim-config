27df20da798ed35c4ae27a2027fd2f167d78e8be
calebesouzas <cacalebehs@gmail.com>
2026-01-23 16:24:48 -0300

    feat(diagnostics): toggle show/hide diagnostics with <leader>d

--- lua/config/environment.lua
+++ lua/config/environment.lua
@@ -2,3 +2,13 @@ vim.env.CC = "gcc"
 vim.env.CXX = "g++"
 
 vim.keymap.set("n", "<leader>w", ":w<CR>")
+vim.keymap.set("n", "<leader>d", function()
+  local disabled = not vim.diagnostic.is_enabled()
+  if disabled then
+    vim.notify("Enabling diagnostics...", vim.log.levels.WARN)
+  else
+    vim.notify("Disabling diagnostics...", vim.log.levels.WARN)
+  end
+
+  vim.diagnostic.enable(disabled)
+end)
